9a1868b40038925f8bcd0422485a969ac93cbc7c
efimikvitaliy <efimikvitaliy@Mac-Admin.local>
2015-10-08 12:44:11 +0300

    Initial Commit

---
+++ TipTipOK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipTipOK
+//
+//  Created by efimikvitaliy on 08.10.15.
+//  Copyright (c) 2015 DreamTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipTipOK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TipTipOK
+//
+//  Created by efimikvitaliy on 08.10.15.
+//  Copyright (c) 2015 DreamTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
